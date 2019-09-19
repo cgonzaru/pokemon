d6fe91e8a5f90a4abaef5a3c7319b675fef939dc
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-19 17:48:51 +0200

    pokemon list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const endpoint = 'https://raw.githubusercontent.com/Adalab/m3-evaluacion-intermedia-oneeyedman/componentbranch/pokemons.json';
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(results => {
+        this.setState({
+          data: results
+        })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul className="pokemon-list">
+          {this.state.data.map((item, index) => {
+            return (
+              <li className="pokemons-card" key={index}>
+                <div className="pokemon-name">{item.name}</div>
+                <img src={item.url} alt={item.name} className="pokemon-img"/>
+                <ul className="pokemons-type">
+                  {item.types.map((type, index) => {
+                    return (
+                      <li className="pokemon-type" key={index}>{type}</li>
+                    );
+                  })}
+                </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
